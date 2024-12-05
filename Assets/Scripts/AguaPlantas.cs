efc73910410a4f648598f3625a6d3258a3fb779b
JorgeCortesThompson <jcortho@iesfuengirola1.es>
2024-12-05 14:06:24 +0100

    Mejorando el script del agua (incompleto y tiene fallos que resolver)

--- Assets/Scripts/AguaPlantas.cs
+++ Assets/Scripts/AguaPlantas.cs
@@ -4,21 +4,47 @@ using UnityEngine.UI;
 
 public class AguaPlantas : MonoBehaviour
 {
-    public float CantidadAgua = 0;
+    public int CantidadAgua = 0;
 
     public Image bar;
     
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    
 
     // Update is called once per frame
     void Update()
     {
-        
-        if(Input.GetKey(KeyCode.Space)){ CantidadAgua += 0.001f; bar.fillAmount = CantidadAgua; }
-    }
-    
-}
+           // Verifica si la barra está llena antes de incrementar el fillAmount y la cantidad de agua
+           if (Input.GetKey(KeyCode.Space) && bar.fillAmount < 1.0f)
+           {                      //0.0002f;
+                                 //0.0006f;
+               bar.fillAmount += 0.00035f;
+   
+               // Solo incrementa CantidadAgua cuando el fillAmount aún no ha llegado a 1
+               if (bar.fillAmount >= 1.0f)
+               {
+                   CantidadAgua = 100;
+               }
+               else
+               {
+                   CantidadAgua = Mathf.RoundToInt(bar.fillAmount * 100);
+               }
+           }
+           if (Input.GetKeyDown(KeyCode.A) && bar.fillAmount != 0f)
+                      {                     
+                          bar.fillAmount -= 0.35f;
+              
+                         
+                          if (bar.fillAmount = 0f)
+                          {
+                              CantidadAgua = 0;
+                          }
+                          else
+                          {
+                              CantidadAgua = CantidadAgua -(Mathf.RoundToInt(bar.fillAmount * 100));
+                          }
+                      }
+       }
+   
+   }
+   
+  
