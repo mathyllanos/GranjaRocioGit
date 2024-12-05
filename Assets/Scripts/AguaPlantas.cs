ef7aaedcf42ae99a0fbcecb7bcf9e3bb661520ea
JorgeCT27 <jcortho@iesfuengirola1.es>
2024-12-05 17:07:09 +0100

    Mejorando el script del agua 2 (incompleto)

--- Assets/Scripts/AguaPlantas.cs
+++ Assets/Scripts/AguaPlantas.cs
@@ -1,23 +1,27 @@
 using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class AguaPlantas : MonoBehaviour
 {
-    public int CantidadAgua = 0;
+   [SerializeField] private int CantidadAgua;
 
     public Image bar;
+    public Text texto;
     
     
 
     // Update is called once per frame
     void Update()
     {
-           // Verifica si la barra está llena antes de incrementar el fillAmount y la cantidad de agua
+            texto.text =   CantidadAgua + " %";
+        // Verifica si la barra está llena antes de incrementar el fillAmount y la cantidad de agua
            if (Input.GetKey(KeyCode.Space) && bar.fillAmount < 1.0f)
            {                      //0.0002f;
                                  //0.0006f;
-               bar.fillAmount += 0.00035f;
+                                 //0.00035f
+               bar.fillAmount +=  0.0006f;
    
                // Solo incrementa CantidadAgua cuando el fillAmount aún no ha llegado a 1
                if (bar.fillAmount >= 1.0f)
@@ -29,22 +33,14 @@ public class AguaPlantas : MonoBehaviour
                    CantidadAgua = Mathf.RoundToInt(bar.fillAmount * 100);
                }
            }
+           //Decrementa el fillAmount,al mismo tiempo decrementando el valor de la CantidadAgua
            if (Input.GetKeyDown(KeyCode.A) && bar.fillAmount != 0f)
-                      {                     
-                          bar.fillAmount -= 0.35f;
-              
-                         
-                          if (bar.fillAmount = 0f)
-                          {
-                              CantidadAgua = 0;
-                          }
-                          else
-                          {
-                              CantidadAgua = CantidadAgua -(Mathf.RoundToInt(bar.fillAmount * 100));
-                          }
-                      }
-       }
+           {                     
+               bar.fillAmount -= 0.25f;
+               CantidadAgua = Mathf.RoundToInt(bar.fillAmount * 100);
+           }
+    }
    
-   }
+}
    
   
