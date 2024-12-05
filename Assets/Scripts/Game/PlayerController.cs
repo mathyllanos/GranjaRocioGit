e957dd98088ad696684791278b17aeb3e6f64bfc
OCG275 <ocangar1702@iesfuengirola1.es>
2024-12-05 19:16:23 +0100

    Udate
    Controlador de Animaciones y controlador de player

--- Assets/Scripts/Game/PlayerController.cs
+++ Assets/Scripts/Game/PlayerController.cs
@@ -9,20 +9,24 @@ using System;
 public class PlayerController: MonoBehaviour
 {
 
-    //Explicacion
+    
 
-
+    //Bariables
     public Rigidbody2D rb;
     public float speed = 5f;
     private Vector2 _input;
     
-    private void Awake()
-    {
-        rb = GetComponent<Rigidbody2D>();
-    }
+    private void Awake(){rb = GetComponent<Rigidbody2D>();}
+
+
+
     void Update()
     {
+        //Explicacion: Esta para es para detectar las teclas que detectan el mobimiento Horizontal y el movimiento Vertical.
+        
         _input = new Vector2(UnityEngine.Input.GetAxisRaw("Horizontal"), UnityEngine.Input.GetAxisRaw("Vertical"));
+       
+        // Explicacion: Cons estos IF determinan cuando no esta quieto y cuando se estan desplazando.
         if (_input.x != 0  ) 
         
             rb.transform.Translate(Vector2.right * (speed * _input.x) * Time.deltaTime);
