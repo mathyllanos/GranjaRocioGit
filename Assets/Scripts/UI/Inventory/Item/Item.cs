632af6ff04a041fd2c80c7d7bd1fb6d4ec120537
HenryDraw <emelrod791@iesfuengirola1.es>
2024-12-06 13:55:12 +0100

    Add: Objectos Scriptables Sistema
    Capacidad de creación de los objetos scriptables, tanto para los vegetales como las herramientas

---
+++ Assets/Scripts/UI/Inventory/Item/Item.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+[CreateAssetMenu(menuName = "ScriptableObject/Item")]
+public class Item : ScriptableObject
+{
+    [Header("Only gameplay")]
+    public TileBase tile;
+    public ItemType type;
+    public ActionType action;
+    public Vector2Int range = new Vector2Int(5, 4);
+
+    [Header("Only UI")]
+    public bool stackable = true;
+
+    [Header("Both")]
+    public Sprite image;
+
+
+    public enum ItemType
+    {
+        Vegetable,
+        Fruit,
+        Tool
+    }
+
+    public enum ActionType
+    {
+        Glove,
+        Hoe,
+        WateringCan
+    }
+}
