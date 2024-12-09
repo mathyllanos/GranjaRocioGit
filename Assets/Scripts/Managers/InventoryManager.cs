9c63123b4f740b00a478fbfc6b37092459fbcf38
HenryDraw <emelrod791@iesfuengirola1.es>
2024-12-09 11:57:46 +0100

    Add: Inventory Manager Codigo
    Adicion del controlador prinicpal de agarre de item del inventario

---
+++ Assets/Scripts/Managers/InventoryManager.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    public InventorySlot[] inventorySlots;
+
+    public GameObject inventoryItemPrefab;
+    public void AddItem(Item item)
+    {
+        //Encuentra slot vacio
+        for (int i = 0; i < inventorySlots.Length; i++)
+        {
+            InventorySlot slot = inventorySlots[i];
+            InventoryItem itemInSlot = slot.GetComponentInChildren<InventoryItem>();
+            if (itemInSlot == null)
+            {
+                SpawnNewItem(item, slot);
+                return;
+            }
+        }
+    }
+
+    void SpawnNewItem(Item item, InventorySlot slot)
+    {
+        GameObject newItemGo = Instantiate(inventoryItemPrefab, slot.transform);
+        InventoryItem inventoryItem = newItemGo.GetComponent<InventoryItem>();
+        inventoryItem.InitialiseItem(item);
+    }
+}
