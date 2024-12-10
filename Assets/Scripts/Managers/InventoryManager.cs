a0a1881146f330e6b897a8ff2878631f90a87125
HenryDraw <emelrod791@iesfuengirola1.es>
2024-12-10 12:28:22 +0100

    Update: Sistema de seleccion Toolbar
    Sistema actualizado para gastar items cuando se usen

--- Assets/Scripts/Managers/InventoryManager.cs
+++ Assets/Scripts/Managers/InventoryManager.cs
@@ -68,4 +68,28 @@ public class InventoryManager : MonoBehaviour
         InventoryItem inventoryItem = newItemGo.GetComponent<InventoryItem>();
         inventoryItem.InitialiseItem(item);
     }
+
+    public Item GetSelectedItem(bool use)
+    {
+        InventorySlot slot = inventorySlots[selectedSlot];
+        InventoryItem itemInSlot = slot.GetComponentInChildren<InventoryItem>();
+        if (itemInSlot != null)
+        {
+            Item item = itemInSlot.item;
+            if (use == true)
+            {
+                itemInSlot.count--;
+                if (itemInSlot.count <= 0)
+                {
+                    Destroy(itemInSlot.gameObject);
+                }
+                else
+                {
+                    itemInSlot.RefreshCount();
+                }
+            }
+            return item;
+        }
+        return null;
+    }
 }
