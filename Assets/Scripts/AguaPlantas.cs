7d5bb1be01f810266cb5f6c7be8769ab4be58d50
JorgeCT27 <jcortho@iesfuengirola1.es>
2024-12-12 21:11:13 +0100

    Update Codigo agua (Terminado)

--- Assets/Scripts/AguaPlantas.cs
+++ Assets/Scripts/AguaPlantas.cs
@@ -10,9 +10,19 @@ public class AguaPlantas : MonoBehaviour
    [SerializeField] private Image bar;
    [SerializeField] private Text texto;
    [SerializeField] private string tocaPozo = "Well";
-   [SerializeField] private string tocaPlanta = "Verduras";
+   [SerializeField] private string tocaPatata = "Patata";
+   [SerializeField] private string tocaZanahoria = "Zanahoria";
+   [SerializeField] private string tocaLechuga = "Lechuga";
+   [SerializeField] private string tocaTomate = "Tomate";
+
    [SerializeField] private bool _puedeLlenar = false;
-   [SerializeField] private bool _regarPlanta = false;
+   [SerializeField] private bool _regarPatata = false;
+   [SerializeField] private bool _regarZanahoria = false;
+   [SerializeField] private bool _regarLechuga = false;
+   [SerializeField] private bool _regarTomate = false;
+
+
+
     
     
 
@@ -26,7 +36,7 @@ public class AguaPlantas : MonoBehaviour
            {                      //0.0002f;
                                  //0.0006f;
                                  //0.00035f
-               bar.fillAmount +=  0.0006f;
+                                 bar.fillAmount += 0.002f;
    
                // Solo incrementa CantidadAgua cuando el fillAmount aún no ha llegado a 1
                if (bar.fillAmount >= 1.0f)
@@ -42,9 +52,24 @@ public class AguaPlantas : MonoBehaviour
            //"_regarPlanta" es verdadero
 
      
-               if (Input.GetKeyDown(KeyCode.E) && bar.fillAmount != 0f && _regarPlanta)
+               if (Input.GetKeyDown(KeyCode.E) && bar.fillAmount != 0f && _regarPatata)
                {                     
-                   bar.fillAmount -= 0.25f;
+                   bar.fillAmount -= 0.1f;
+                   CantidadAgua = Mathf.RoundToInt(bar.fillAmount * 100);
+               }
+               if (Input.GetKeyDown(KeyCode.E) && bar.fillAmount != 0f && _regarZanahoria)
+               {                     
+                   bar.fillAmount -= 0.3f;
+                   CantidadAgua = Mathf.RoundToInt(bar.fillAmount * 100);
+               }
+               if (Input.GetKeyDown(KeyCode.E) && bar.fillAmount != 0f && _regarLechuga)
+               {                     
+                   bar.fillAmount -= 0.5f;
+                   CantidadAgua = Mathf.RoundToInt(bar.fillAmount * 100);
+               }
+               if (Input.GetKeyDown(KeyCode.E) && bar.fillAmount != 0f && _regarTomate)
+               {                     
+                   bar.fillAmount -= 0.75f;
                    CantidadAgua = Mathf.RoundToInt(bar.fillAmount * 100);
                }
                
@@ -59,18 +84,36 @@ public class AguaPlantas : MonoBehaviour
             _puedeLlenar = true; // Permite que la barra pueda llenarse si contacta con un collider de un objeto de tag
             //"well" (pozo)
         }
-        if (other.gameObject.CompareTag("Verduras"))
+        // Permite que la barra pueda bajarse si contacta con un collider de un objeto de tag
+        //"(verduras)"
+        if (other.gameObject.CompareTag("Patata"))
         {
-            _regarPlanta = true; // Permite que la barra pueda bajarse si contacta con un collider de un objeto de tag
-            //"verduras"
+            _regarPatata = true; 
         }
+        if (other.gameObject.CompareTag("Zanahoria"))
+        {
+            _regarZanahoria = true; 
+        }
+        if (other.gameObject.CompareTag("Lechuga"))
+        {
+            _regarLechuga = true; 
+        }
+        if (other.gameObject.CompareTag("Tomate"))
+        {
+            _regarTomate = true; 
+        }
+        
     }
     private void OnCollisionExit2D(Collision2D other)
     {
         if (other != null)
         {
             _puedeLlenar = false; // Desactiva la posibilidad de llenar la barra
-            _regarPlanta = false;// Desactiva la posibilidad de bajar la barra
+            // Desactiva la posibilidad de bajar la barra
+            _regarPatata = false;
+            _regarZanahoria = false;
+            _regarLechuga = false;
+            _regarTomate = false;
         }
     }
    
