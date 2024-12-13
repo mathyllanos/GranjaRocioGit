4af7692fd656fc20a2267c3874495fd8f1dbe62b
OCG275 <ocangar1702@iesfuengirola1.es>
2024-12-13 16:39:57 +0100

    Add,Udate
    He añadido nuebos sprites y ecrado un escri bara el control de camas

--- Assets/Scripts/Game/PlayerAnimator.cs
+++ Assets/Scripts/Game/PlayerAnimator.cs
@@ -17,27 +17,63 @@ public class PlayerAnimator : MonoBehaviour
     private Vector2 _ultimaPosicion;
     private Animator _animator;
     private void Awake(){ _animator = GetComponent<Animator>();}
+    public bool x, y;
+
+
+
+
     void Update()
     {
         _input = new Vector2(UnityEngine.Input.GetAxisRaw("Horizontal"), UnityEngine.Input.GetAxisRaw("Vertical"));
-        if (_input.x != 0)
+
+
+
+
+        if (x == false)
         {
-            _animator.SetBool("Quieto", false);
-            _ultimaPosicion.y = 0;
-            _ultimaPosicion.x = _input.x;
+            if (_input.x != 0)
+            {
+                _animator.SetBool("Quieto", false);
+                _ultimaPosicion.y = 0;
+                _ultimaPosicion.x = _input.x;
+                y = true;
+            }
+            else 
+            {
+                y = false;
+
+            }
         }
 
-        if (_input.y != 0) 
+        if (y == false)
         {
-            _animator.SetBool("Quieto", false);
-            _ultimaPosicion.x = 0;
-            _ultimaPosicion.y = _input.y;
+            if (_input.y != 0)
+            {
+                _animator.SetBool("Quieto", false);
+                _ultimaPosicion.x = 0;
+                _ultimaPosicion.y = _input.y;
+                x = true;
+            }
+            else 
+            { 
+            
+                x = false ;
+            }
+
+
+
         }
 
-        if(_input.x == 0 && _input.y == 0) 
+
+
+        if (_input.x == 0 && _input.y == 0)
         {
             _animator.SetBool("Quieto", true);
         }
+
+
+
+
         _animator.SetFloat("AndarX", _input.x);
         _animator.SetFloat("AndarY", _input.y);
         _animator.SetFloat("QuietoAndarX", _ultimaPosicion.x);
