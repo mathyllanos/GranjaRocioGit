0e24729f0b5937f115801e7b59ac0a4be6010fa7
aoregil <aorell92@gmail.com>
2024-12-15 01:39:08 +0100

    Update: Condiciones para las animaciones en el código del Sistema de cultivo
    Añadidas las condiciones para que se ejecuten las animaciones correspondientes a las diferentes acciones.

--- Assets/Scripts/Game/CultivateGround.cs
+++ Assets/Scripts/Game/CultivateGround.cs
@@ -15,25 +15,38 @@ public class CultivateGround : MonoBehaviour
     public int pickedSeed = -1;
     public InventoryManager tool;
 
+    public Animator playerAnimator;
+
     private void Update()
     {
-        if (currentCell != null && Input.GetKeyDown(KeyCode.Space))
+        if (currentCell != null && Input.GetKeyDown(KeyCode.E))
         {
             // Si el terreno es arable y se tiene la azada seleccionada, creamos el terreno arado
             if (currentCell.CompareTag("Arable") && tool.selectedSlot == 5)
             {
+                playerAnimator.SetInteger("Herramienta", 1);
+                playerAnimator.SetBool("Interactuar", true);
+                Invoke("RestoreAnimations", 2);
                 Instantiate(plowedGroundPrefab, currentCell.transform.position, quaternion.identity);
                 Destroy(currentCell.gameObject);
             }
             // Si ya está arado y se tienen los guantes seleccionados, se crea un terreno sembrado según la semilla que se haya recogido 
             else if (currentCell.CompareTag("Plowed") && tool.selectedSlot == 4 && pickedSeed >= 0)
             {
+                playerAnimator.SetInteger("Herramienta", 0);
+                playerAnimator.SetBool("Interactuar", true);
+                Invoke("RestoreAnimations", 2);
                 Instantiate(sownGroundPrefabs[pickedSeed], currentCell.transform.position, quaternion.identity);
                 pickedSeed = -1;
                 Destroy(currentCell.gameObject);
             }
             else if (currentCell.CompareTag("Seed") && tool.selectedSlot == 4)
+            {
+                playerAnimator.SetInteger("Herramienta", 0);
+                playerAnimator.SetBool("Interactuar", true);
+                Invoke("RestoreAnimations", 2);
                 pickedSeed = currentCell.GetComponent<Seed>().id;
+            }
             
         }
     }
@@ -48,5 +61,10 @@ public class CultivateGround : MonoBehaviour
     {
         if (other.CompareTag("Arable") || other.CompareTag("Plowed"))
             currentCell = null;
+    }
+
+    private void RestoreAnimations()
+    {
+        playerAnimator.SetBool("Interactuar", false);
     }
 }
