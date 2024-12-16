d15966b46fed9dd21123f9a4b457bee6d31b575c
aoregil <aorell92@gmail.com>
2024-12-16 21:27:53 +0100

    Update: Cambiado sistema de misiones
    Al no haberse logrado implementar el sistema de misiones a tiempo, hemos hecho que simplemente se gane puntuación según lo que se recoja.

--- Assets/Scripts/Game/Misiones.cs
+++ Assets/Scripts/Game/Misiones.cs
@@ -1,154 +1,73 @@
+using UnityEditor;
 using UnityEngine;
 
 public class Misiones : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
+    public GameObject CanvasWin;
+    public GameObject CanvasLose;
 
-    [SerializeField] float Duracion;
-
-   private float time;
-   [SerializeField] private float inter;
-   private float ac_time;
-    private bool Denido;
+    [SerializeField] float Duracion; 
+    
+    private float time; 
+    [SerializeField] private float inter; 
+    private float ac_time;
+    private bool detenido;
     public GameObject Aguja;
     public int Conteo;
 
-
+    public CultivateGround cultivateGround;
     
-
     void Detener() 
     {
-        Denido = true;
+        detenido = true;
+        
+        if (cultivateGround.puntos >= 250)
+            CanvasWin.SetActive(true);
 
-        Debug.Log("Fin_Del_tiempo");
-        Time.timeScale = 0;
-
-
+        else
+            CanvasLose.SetActive(true);
     }
-
-
-    void Tiemer() 
+    
+    /* void Tiemer() 
     {
-
-
         time += Time.deltaTime;
         inter = time;
         inter = time - ac_time;
-
-
-
+        
         Aguja.transform.Rotate(Vector3.back * (360/Duracion) * Time.deltaTime);
-
-
-
-
-
-
-    }
-
-
-
-
-
-
-
-
-
-    void Start()
-    {
-        Invoke("Tiks", (Duracion / 24));
-    }
-
+    }*/
+    
     void Tiks() 
     {
         if (Conteo < 24) 
         {
-
             Aguja.transform.Rotate(Vector3.back * 15);
 
             Conteo++;
 
-
             Invoke("Tiks", (Duracion / 24));
         }
-
-        
     }
-
     
-
-
-    // Update is called once per frame
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    void Update()
+    void Start()
     {
-
-
-        if (!Denido) 
+        Invoke("Detener", Duracion);
+        Invoke("Tiks", (Duracion / 24));
+    }
+    
+    /* void Update()
+    {
+        if (!detenido) 
         {
 
-
-          
-
-
         }
-        Invoke("Detener", Duracion);
-
 
         if (Input.GetKeyDown(KeyCode.R)) 
         {
-
             ac_time = time;
-
-
         }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    }
-
-
-
-
-
-
-
-
-
-
-
-
+    }*/
+    
 }
